90c00d158e6c2bb873e1d0325b24de06dfb381f6
Andrew Norris <norrisa@faytechcc.edu>
2019-01-15 14:18:58 -0500

    First upload

---
+++ M1T1_Norris/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// CSC 134
+// M1T1
+// Norrisa
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from CSC134!" << endl;
+    cout << "This is some more text" << endl;
+    return 0;
+}
