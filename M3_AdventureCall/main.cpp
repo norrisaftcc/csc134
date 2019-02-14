4e4e299502c6d4291362da3eeef994d28a9af238
Andrew Norris <norrisa@faytechcc.edu>
2019-02-14 13:48:00 -0500

    Updated key code for the shack

--- M3_AdventureCall/main.cpp
+++ M3_AdventureCall/main.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 /*
+CSC 134
+M3HW
+Norris
+2/14/19
+
 Adventure (function) call version 1.1
 Last updated 2/14/19
 
@@ -26,13 +31,14 @@ void insideShack();
 // list global variables here
 // (we would rather not use globals
 // but they'll work for now)
-bool firstPlay = true;
-bool hasKey = false;
+bool firstPlay = true; // is this first playthrough
+bool hasKey = false;   // do they have the key?
 
 
 int main()
 {
     // this is probably more complex than needed
+
     char again;
     cout << "Welcome, traveler!" << endl;
     if (firstPlay) {
@@ -61,7 +67,18 @@ int main()
 Garden - leads to gazebo, shack, and well.
 */
 void garden() {
-
+    cout << "You are in the garden." << endl;
+    cout << "A shack is to the EAST." << endl;
+    cout << "there is a KEY here on the ground." << endl;
+    string command;
+    cin >> command;
+    if (command == "east") {
+        outsideShack();
+    }
+    else if (command == "key") {
+        hasKey = true;
+        garden();
+    }
 }
 
 
@@ -83,12 +100,24 @@ void well() {
 Outside Shack - leads back to garden or inside shack to victory
 */
 void outsideShack() {
-
+    string command;
+    cout << "You are outside of a shack." << endl;
+    cout << "You can return WEST to the garden." << endl;
+    cout << "The DOOR is locked firmly." << endl;
+    cin >> command;
+    if (command == "door") {
+        if (hasKey) {
+            insideShack();
+        }
+        else {
+            cout << "The door is locked." << endl;
+        }
+    }
 }
 
 /*
 Inside Shack - win condition
 */
 void insideShack() {
-
+    cout << "The treasure is here -- you win!" << endl;
 }
